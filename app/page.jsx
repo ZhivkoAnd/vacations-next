9d298bba906896efafc15bc7449f16e4c33873c1
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-11-26 20:25:33 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,10 @@ const fetchVacations = async () => {
     accessToken: "qSxY7HTMgBYn3WQP4bL5svs27iUAQZEM-rauSvhvixg",
   });
 
-  const response = await client.getEntries({ content_type: "recipe" });
+  const response = await client.getEntries(
+    { content_type: "recipe" },
+    { next: { revalidate: 1 } }
+  );
   return response;
 };
 
@@ -17,27 +20,30 @@ const Vacations = async () => {
   const vacations = await fetchVacations();
 
   return (
-    <div className="vacation-panels">
-      {vacations?.items.map((item) => (
-        <div className="vacation-panel" key={item.sys.id}>
-          <Image
-            src={`https:${item.fields.thumbnail.fields.file.url}`}
-            width={item.fields.thumbnail.fields.file.details.image.width}
-            height={item.fields.thumbnail.fields.file.details.image.height}
-            className="vacation-panel__image"
-            alt="city thumbnail"
-          />
-          <h2 className="vacation-panel__title"> {item.fields.title}</h2>
-          <h3 className="vacation-panel__date"> {item.fields.date}</h3>
-          <Link
-            href={`/vacations/${item.fields.slug}`}
-            className="btn btn-primary vacation-panel__button"
-          >
-            Gallery
-          </Link>
-        </div>
-      ))}
-    </div>
+    <>
+      <h1 className="title-main">Vacations</h1>
+      <div className="vacation-panels">
+        {vacations?.items.map((item) => (
+          <div className="vacation-panel" key={item.sys.id}>
+            <Image
+              src={`https:${item.fields.thumbnail.fields.file.url}`}
+              width={item.fields.thumbnail.fields.file.details.image.width}
+              height={item.fields.thumbnail.fields.file.details.image.height}
+              className="vacation-panel__image"
+              alt="city thumbnail"
+            />
+            <h2 className="vacation-panel__title"> {item.fields.title}</h2>
+            <h3 className="vacation-panel__date"> {item.fields.date}</h3>
+            <Link
+              href={`/vacations/${item.fields.slug}`}
+              className="btn btn-primary vacation-panel__button"
+            >
+              Gallery
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
