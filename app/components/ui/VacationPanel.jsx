578becac883bc51e0a36210a8297e08aa21d4dca
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-11-26 20:27:46 +0100

    changes

---
+++ app/components/ui/VacationPanel.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const VacationPanel = ({ item }) => {
+  return (
+    <div className="vacation-panel" key={item.sys.id}>
+      <Image
+        src={`https:${item.fields.thumbnail.fields.file.url}`}
+        width={item.fields.thumbnail.fields.file.details.image.width}
+        height={item.fields.thumbnail.fields.file.details.image.height}
+        className="vacation-panel__image"
+        alt="city thumbnail"
+      />
+      <h2 className="vacation-panel__title"> {item.fields.title}</h2>
+      <h3 className="vacation-panel__date"> {item.fields.date}</h3>
+      <Link
+        href={`/vacations/${item.fields.slug}`}
+        className="btn btn-primary vacation-panel__button"
+      >
+        Gallery
+      </Link>
+    </div>
+  );
+};
+
+export default VacationPanel;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createClient } from "contentful";
-import Image from "next/image";
-import Link from "next/link";
+import VacationPanel from "./components/ui/VacationPanel";
 
 const fetchVacations = async () => {
   const client = createClient({
@@ -23,24 +22,8 @@ const Vacations = async () => {
     <>
       <h1 className="title-main">Vacations</h1>
       <div className="vacation-panels">
-        {vacations?.items.map((item) => (
-          <div className="vacation-panel" key={item.sys.id}>
-            <Image
-              src={`https:${item.fields.thumbnail.fields.file.url}`}
-              width={item.fields.thumbnail.fields.file.details.image.width}
-              height={item.fields.thumbnail.fields.file.details.image.height}
-              className="vacation-panel__image"
-              alt="city thumbnail"
-            />
-            <h2 className="vacation-panel__title"> {item.fields.title}</h2>
-            <h3 className="vacation-panel__date"> {item.fields.date}</h3>
-            <Link
-              href={`/vacations/${item.fields.slug}`}
-              className="btn btn-primary vacation-panel__button"
-            >
-              Gallery
-            </Link>
-          </div>
+        {vacations?.items.map((vacation) => (
+          <VacationPanel item={vacation} />
         ))}
       </div>
     </>
