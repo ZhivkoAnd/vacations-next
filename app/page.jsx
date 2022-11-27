4996f35ab252cfd4fac14e4266e504519f6326ad
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-11-27 23:55:57 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,41 @@
 "use client";
 
-import React from "react";
 import VacationPanel from "./components/ui/VacationPanel";
 import { fetchVacationsClient } from "./components/utils/FetchQueryClient";
+import { useState, useEffect } from "react";
 
 const Vacations = () => {
-  const { data } = fetchVacationsClient();
+  const { data, isLoading } = fetchVacationsClient();
+
+  const [query, setQuery] = useState("");
+  const [filteredCities, setFilteredCities] = useState(data);
+  const [noVacationFound, setNoVacationFound] = useState(false);
+
+  const inputCity = data?.items.filter((city) =>
+    city.fields.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  useEffect(() => {
+    if (inputCity && inputCity.length) {
+      setFilteredCities(inputCity);
+      setNoVacationFound(false);
+    } else {
+      setFilteredCities([]);
+      setNoVacationFound(true);
+    }
+  }, [query, data]);
+
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
 
   return (
     <>
       <h1 className="title-main">Vacations</h1>
+      <input value={query} onChange={(e) => setQuery(e.target.value)}></input>
+      {noVacationFound ? <div>NOTHING HERE</div> : ""}
       <div className="vacation-panels">
-        {data?.items.map((vacation) => (
+        {filteredCities?.map((vacation) => (
           <VacationPanel item={vacation} key={vacation.sys.id} />
         ))}
       </div>
