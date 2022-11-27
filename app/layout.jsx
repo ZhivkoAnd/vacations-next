cf01a297028f046476d2817320d97bc6db37a842
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-11-27 12:40:02 +0100

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,38 +1,20 @@
-"use client";
-
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.scss";
 import "../styles/Navigation.scss";
 import "../styles/Footer.scss";
 import "../styles/VacationPanel.scss";
-
+import Wrap from "./Wrap";
 import Providers from "./Providers";
-import Navigation from "./components/ui/Navigation";
-import Footer from "./components/ui/Footer";
-
-import { useState } from "react";
 
 export default function RootLayout({ children }) {
-  const [colorMode, setColorMode] = useState("dark");
-
-  const lightMode = () => {
-    setColorMode("light");
-  };
-
-  const darkMode = () => {
-    setColorMode("dark");
-  };
-
   return (
     <html>
       <head />
-      <body className={`App ${colorMode}`}>
+      <body>
         <Providers>
-          <Navigation lightMode={lightMode} darkMode={darkMode} />
-          <div className="page-content">
+          <Wrap>
             <div className="container">{children}</div>
-          </div>
-          <Footer />
+          </Wrap>
         </Providers>
       </body>
     </html>
